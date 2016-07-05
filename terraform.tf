5aa1682c8c8295e2f29293db573d7f152e9a6b7d
namikingsoft <namikingsoft@gmail.com>
2016-07-06 07:14:54 +0900

    Revert "S3サーバーからFastlyの通信をSSLにしてみる"

--- terraform.tf
+++ terraform.tf
@@ -90,7 +90,7 @@ resource "fastly_service_v1" "cdn" {
   backend {
     address = "${aws_s3_bucket.origin.website_endpoint}"
     name    = "AWS S3 hosting"
-    port    = 443
+    port    = 80
   }
 
   gzip {
